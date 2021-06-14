2c6c2e82263fb8e57ddf15ba189e15b743fe1566
Rachel Colquhoun <rachel.colquhoun@ed.ac.uk>
2021-06-14 14:33:43 +0100

    typo

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ import os
 import pkg_resources
 from version import __version__, _program
 
-setup(name='pango-designation',
+setup(name='pango_designation',
       version=__version__,
       packages=find_packages(),
       scripts=[],
-      package_data={'pango-designation':['alias_key.json']},
+      package_data={'pango_designation':['alias_key.json']},
       description='PANGO lineage designations for SARS-CoV-2',
       url='https://github.com/cov-lineages/pango-designation',
       author='cov-lineages group',
