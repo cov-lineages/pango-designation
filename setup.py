6965a2284c7f41b0b51da2f387a19a1797a2d978
Rachel Colquhoun <rachel.colquhoun@ed.ac.uk>
2021-06-14 15:09:14 +0100

    still trying to make installable

--- setup.py
+++ setup.py
@@ -2,20 +2,13 @@ from setuptools import setup, find_packages
 import glob
 import os
 import pkg_resources
-from version import __version__, _program
+from pango_designation import __version__, _program
 
-setup(name='pango_designation',
+setup(name=_program,
       version=__version__,
-      packages=find_packages(),
-      scripts=[],
-      package_data={'pango_designation':['alias_key.json']},
+      package_data={'':['alias_key.json']},
       description='PANGO lineage designations for SARS-CoV-2',
       url='https://github.com/cov-lineages/pango-designation',
       author='cov-lineages group',
-      entry_points="""
-      [console_scripts]
-      {program} = pango_designation.command:main
-      """.format(program = _program),
       include_package_data=True,
-      keywords=[],
       zip_safe=False)
