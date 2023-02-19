a3cf13e874b5dff6c8b58378ae0f3d99531ddf25
Cornelius Roemer <cornelius.roemer@gmail.com>
2023-02-19 20:07:40 +0100

    Add deduplicate_keeping_first.py script to add extra designations without touching sublineages

---
+++ deduplicate_keeping_first.py
@@ -0,0 +1,38 @@
+# Need to install `llist` package for this to work
+from llist import dllist
+# Read in file from end to start
+
+d = dllist()
+with open('lineages.csv', 'r') as f:
+    for line in f:
+        d.append(line)
+
+#%%
+# Iterate over each line in d from back to front
+# Save the string till first comma in hashset
+# If string in hashset, delete line 
+hashset = set()
+
+keep_going=True
+next = d.first
+while next:
+    current = next
+    next = next.next
+    line = current.value
+    split = line.split(',')
+    if line == '\n':
+        d.remove(current)
+        continue
+    if split[0] in hashset:
+        d.remove(current)
+        print(f"Removed duplicate strain: {line}")
+        continue
+    hashset.add(split[0])
+    
+
+#%%
+# Write back to file
+with open('lineages.csv', 'w') as f:
+    f.writelines(d)
+    f.write('\n')
+
